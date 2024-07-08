93b8d597dacdceebe41d8984292f08cc63043f32
Michael <mike@sokpuppet.org>
2024-07-08 17:05:06 -0500

    After ListMaterials

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ import sqlite3
 
 app = FastAPI()
 
-class resp(BaseModel):
+class print(BaseModel):
     name: str
     id: int
     gid: int
+
+class material(BaseModel):
+    name: str
+    id: int
+    gid: int
+    amount: int
+
 
 @app.get("/v1/ListPrints")
 def index():
@@ -19,9 +26,19 @@ def index():
     prints = []
 
     for row in cur.execute("SELECT typeName, typeID, groupID from invTypes WHERE typeNAME like '%blueprint'"):
-        prints.append(resp(name=row[0], id=row[1], gid=row[2]))
+        prints.append(print(name=row[0], id=row[1], gid=row[2]))
         
 
     prints_json = jsonable_encoder(prints)
     return JSONResponse(content=prints_json)
-    
+
+@app.get("/v1/ListMaterials")
+def ListMaterials(PrintID: int):
+    Materials = []
+    con = sqlite3.connect("sqlite-latest.sqlite")
+    cur=con.cursor()
+    query = "SELECT iam.materialTypeID,it.typename,it.groupid,iam.quantity FROM industryactivitymaterials iam join invtypes it on iam.materialtypeid = it.typeid WHERE iam.typeid = " + str(PrintID)
+    for row in cur.execute(query):
+        Materials.append(material(name=row[1], id=row[0], gid=row[2], amount=row[3]))
+    materials_json = jsonable_encoder(Materials)
+    return JSONResponse(content=materials_json)
